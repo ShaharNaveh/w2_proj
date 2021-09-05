a1f79467c4bdf2672a390b8bccd87ee7b2ae7260
ShaharNaveh <>
2021-09-06 02:00:17 +0300

    Make sure password contains uppercase and lowercase

--- password_validator.ps1
+++ password_validator.ps1
@@ -8,3 +8,11 @@ if ( $PASS -notmatch '\d+' -or $PASS -notmatch '[a-zA-Z]+' ) {
 
     Write-Host "Error: Does not contain both numbers and letters"
 }
+
+if ( $PASS -notmatch '(?-i)[a-z]+' ) {
+    Write-Host "Error: Does not contain a lowercase letter"
+}
+
+if ( $PASS -notmatch '(?-i)[A-A]+' ) {
+    Write-Host "Error: Does not contain an upercase letter"
+}
