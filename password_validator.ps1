87716885e5f93896c3e318cafa798d8959c06789
ShaharNaveh <>
2021-09-06 02:04:58 +0300

    Color output

--- password_validator.ps1
+++ password_validator.ps1
@@ -1,18 +1,18 @@
 $PASS=$args[0]
 
 if( -not ($PASS.Length -ge 10)) {
-    Write-Host "Error: Password is less than 10 characters."
+    Write-Host "Error: Password is less than 10 characters." -ForegroundColor Red
 }
 
 if ( $PASS -notmatch '\d+' -or $PASS -notmatch '[a-zA-Z]+' ) {
 
-    Write-Host "Error: Does not contain both numbers and letters"
+    Write-Host "Error: Does not contain both numbers and letters" -ForegroundColor Red
 }
 
 if ( $PASS -notmatch '(?-i)[a-z]+' ) {
-    Write-Host "Error: Does not contain a lowercase letter"
+    Write-Host "Error: Does not contain a lowercase letter" -ForegroundColor Red
 }
 
 if ( $PASS -notmatch '(?-i)[A-A]+' ) {
-    Write-Host "Error: Does not contain an upercase letter"
+    Write-Host "Error: Does not contain an upercase letter" -ForegroundColor Red
 }
