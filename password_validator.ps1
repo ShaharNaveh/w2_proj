282a445d3a1ecf3cdf9f117cb42e5b9eae8603d1
ShaharNaveh <>
2021-09-06 02:06:13 +0300

    Exit status on failure

--- password_validator.ps1
+++ password_validator.ps1
@@ -2,17 +2,22 @@ $PASS=$args[0]
 
 if( -not ($PASS.Length -ge 10)) {
     Write-Host "Error: Password is less than 10 characters." -ForegroundColor Red
+    exit 1
 }
 
 if ( $PASS -notmatch '\d+' -or $PASS -notmatch '[a-zA-Z]+' ) {
-
     Write-Host "Error: Does not contain both numbers and letters" -ForegroundColor Red
+    exit 1
 }
 
 if ( $PASS -notmatch '(?-i)[a-z]+' ) {
     Write-Host "Error: Does not contain a lowercase letter" -ForegroundColor Red
+    exit 1
 }
 
 if ( $PASS -notmatch '(?-i)[A-A]+' ) {
     Write-Host "Error: Does not contain an upercase letter" -ForegroundColor Red
+    exit 1
 }
+
+exit 0
