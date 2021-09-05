bbb88f5046b3831468b8f647e9f531ebbb426b7c
ShaharNaveh <>
2021-09-06 01:54:40 +0300

    Error if not both letters and numbers

--- password_validator.ps1
+++ password_validator.ps1
@@ -3,3 +3,8 @@ $PASS=$args[0]
 if( -not ($PASS.Length -ge 10)) {
     Write-Host "Error: Password is less than 10 characters."
 }
+
+if ( $PASS -notmatch '\d+' -or $PASS -notmatch '[a-zA-Z]+' ) {
+
+    Write-Host "Error: Does not contain both numbers and letters"
+}
